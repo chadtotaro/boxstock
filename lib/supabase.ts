8bb4af13c314edfa440d86458c54e2c17090ee2e
Chad Totaro <chadtotaro@gmail.com>
2026-02-19 20:01:27 -0700

    Hardcode Supabase fallback for Vercel

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? 'https://xjlhmuryrckfecmpgqwa.supabase.co'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? 'sb_publishable_x49Ec6cXNSibutmjiw10Jw_Vs2cYtV8'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
