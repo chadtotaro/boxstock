657995815a91c438d9d36e7b6bd9873cbbec11eb
Chad Totaro <chadtotaro@gmail.com>
2026-02-21 13:03:14 -0700

    Fix Supabase anon key

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? 'https://xjlhmuryrckfecmpgqwa.supabase.co'
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? 'sb_publishable_x49Ec6cXNSibutmjiw10Jw_Vs2cYtV8'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqbGhtdXJ5cmNrZmVjbXBncXdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzMDEwNzIsImV4cCI6MjA4Njg3NzA3Mn0.xfg4Gm5_99vmMs2P5ujeFHt9y-2GHEs1iFFNVj7HPiE'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
