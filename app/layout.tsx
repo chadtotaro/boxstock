f08d7a35a1208af1c431ea061b70087915fdf5b0
Chad Totaro <chadtotaro@gmail.com>
2026-03-03 20:17:59 -0700

    Fix: use ClientProviders import in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import GlobalHeader from "@/components/GlobalHeader";
-import { ThemeProvider } from "@/components/builder/core/hooks/useThemeContext";
+import ClientProviders from "@/components/ClientProviders";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
